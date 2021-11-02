6fd9b312deb090b125da496681d9f378f44b1303
DCurrie07 <demarcus.currie@yahoo.com>
2021-11-01 20:33:37 -0500

    update

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
-from flask import Flask
-import pymysql
+from flask import Flask, request
+import pymysql 
 
 app = Flask(__name__)
 
-@app.route('/admin')
-def mysqlconnect():
-	# To connect MySQL database
-	conn = pymysql.connect(
-		host='us-cdbr-east-04.cleardb.com',
-		user='b80e6664a2dc60',
-		password = "79eb48e5",
-		db='heroku_f1cb21ef63afad5',
-		)
-	
-	cur = conn.cursor()
-	
-	# Select query
-	cur.execute("select * from admin")
-	output = cur.fetchall()
-	
-	for i in output:
-		print(i)
-	
-	# To close the connection
-	conn.close()
+app.config['MYSQL_USER'] = 'sql5448320'
+app.config['MYSQL_PASSWORD'] = '1HYmpBYzky'
+app.config['MYSQL_HOST'] = 'sql5.freemysqlhosting.net'
+app.config['MYSQL_DB'] = 'sql5448320'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-# Driver Code
-if __name__ == "__main__" :
-	mysqlconnect()
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor = pymysql.connection.cursor()
+        pymysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
